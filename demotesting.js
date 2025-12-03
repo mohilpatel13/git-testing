2a2c29bc86fc052ddf63d2ab209d89aa5e3e8e82
mohilpatel13 <pmohil138@gmail.com>
2025-12-03 20:56:34 +0530

    This is dummy commit 1

---
+++ demotesting.js
@@ -0,0 +1,4 @@
+print("this is demotesting.js");
+function demoFunction() {
+    return "Demo function executed";
+}
---
+++ demotesting2.js
@@ -0,0 +1,4 @@
+print("this is demotesting2.js");
+function demoFunction() {
+    return "Demo function executed";
+}
